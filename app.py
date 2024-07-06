2e601d297edfdfa520632fa3043e8efc786850a9
Qiniso Xulu <qiniso.nhlakanipho.xulu@gmail.com>
2024-07-06 14:41:50 +0200

    Second commit

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello World")
+class Person():
+    def __init__ (self, name):
+        self.name = name
+    
+    def talk(self):
+        print("talked")
+
+first_person = Person("Qiniso")
+first_person.talk()
