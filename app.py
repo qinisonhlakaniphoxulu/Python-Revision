fbeb54b571fa0de967777738441f199a784b74c2
qinisonhlakaniphoxulu <qiniso.nhlakanipho.xulu@gmail.com>
2024-07-07 11:46:40 +0200

    "Last commit"

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-class Person():
+import converters
+from converters import kg_to_lbs, lbs_to_kg
+from utils import find_max
+from ecommerce.shipping import shipping_cost
+from pathlib import Path
+
+
+#DEFINING A CLASS
+class Person:
     def __init__ (self, name):
         self.name = name
     
@@ -7,3 +15,51 @@ class Person():
 
 first_person = Person("Qiniso")
 first_person.talk()
+
+#INHERITANCE
+
+class Mammal:
+    def walk(self):
+        print("walking")
+
+class Dog(Mammal):
+    def bark(self):
+        print("barking")
+
+class German_Sherpard(Dog):
+    pass
+
+class Favourate_Pet():
+    def myPet(self):
+        print("Pet")
+class Cat(Mammal, Favourate_Pet):
+    def mow(self):
+        print("miyyaw")
+
+sherpard = German_Sherpard()
+sherpard.walk()
+cat = Cat() 
+cat.walk()
+cat.mow()
+cat.myPet()
+
+
+#MODULES
+#print(converters.lbs_to_kg(70))
+
+print(kg_to_lbs(50))
+print(lbs_to_kg(50))
+
+numbers = [2, 3, "1"]
+
+print(find_max(numbers))
+
+
+print(shipping_cost())
+
+path = Path('C:/Users/Qiniso Xulu')
+print(path.exists())
+
+
+for file in path.glob('*'):
+    print(file)
